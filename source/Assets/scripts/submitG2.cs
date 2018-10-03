028f2a6c183aae729f0b0406bb942929ed2094e4
Amrendra Singh <ashking13th@gmail.com>
2018-10-03 23:11:08 +0530

    Source added

---
+++ source/Assets/scripts/submitG2.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class submitG2 : MonoBehaviour
+{
+    public Text p1, p2;
+
+    string CreateUserUrl = "addAI.php";
+
+    void Update()
+    {
+        //Debug.Log(p1.text);
+
+        //Debug.Log(p2.text);
+    }
+
+    public void CreateUser()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("p1", p1.text);
+        form.AddField("p2", p2.text);
+        form.AddField("gid", 2);
+
+        WWW www = new WWW(CreateUserUrl, form);
+    }
+}
+
